057f7ac151496b3b532fbe6e0824ce287d84054e
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-09-19 07:43:09 +0800

    Move everything up, no more web dir

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	static const char *scope_rel = "/shopify.app.toml";
 	char scope[app_dir_len + strlen(scope_rel) + 1];
 	sprintf(scope, "%s%s", APP_DIR, scope_rel);
-	static const char *index_rel = "/web/frontend/index.html";
+	static const char *index_rel = "/frontend/index.html";
 	char index[app_dir_len + strlen(index_rel) + 1];
 	sprintf(index, "%s%s", APP_DIR, index_rel);
 	shopify_app(API_KEY, API_SECRET_KEY, APP_URL, "/auth", APP_ID, scope,

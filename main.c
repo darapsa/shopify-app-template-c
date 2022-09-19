3cd8a664ac4cc103e5961e0edcc4c0d567546c58
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-09-19 20:42:17 +0800

    Rename for consistency

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 int main(int argc, char *argv[])
 {
 	const size_t app_dir_len = strlen(APP_DIR);
-	static const char *scope_rel = "/shopify.app.toml";
-	char scope[app_dir_len + strlen(scope_rel) + 1];
-	sprintf(scope, "%s%s", APP_DIR, scope_rel);
+	static const char *scopes_rel = "/shopify.app.toml";
+	char scopes[app_dir_len + strlen(scopes_rel) + 1];
+	sprintf(scopes, "%s%s", APP_DIR, scopes_rel);
 	static const char *index_rel = "/frontend/index.html";
 	char index[app_dir_len + strlen(index_rel) + 1];
 	sprintf(index, "%s%s", APP_DIR, index_rel);
-	shopify_app(API_KEY, API_SECRET_KEY, APP_URL, "/auth", APP_ID, scope,
+	shopify_app(API_KEY, API_SECRET_KEY, APP_URL, "/auth", APP_ID, scopes,
 			index, (struct shopify_api[]){
 			{
 				"/products",

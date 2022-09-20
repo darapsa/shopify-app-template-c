7dc3cc30aaeb68b9266fffd65c3d39a277197027
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-09-20 16:18:57 +0800

    Pass WASM (& JS) build dir as JavaScript directory

--- main.c
+++ main.c
@@ -11,8 +11,11 @@ int main(int argc, char *argv[])
 	static const char *index_rel = "/frontend/index.html";
 	char index[app_dir_len + strlen(index_rel) + 1];
 	sprintf(index, "%s%s", APP_DIR, index_rel);
+	static const char *js_dir_rel = "/frontend/build";
+	char js_dir[app_dir_len + strlen(js_dir_rel) + 1];
+	sprintf(js_dir, "%s%s", APP_DIR, js_dir_rel);
 	shopify_app(API_KEY, API_SECRET_KEY, APP_URL, "/auth", APP_ID, scopes,
-			index, (struct shopify_api[]){
+			index, js_dir, (struct shopify_api[]){
 			{
 				"/products",
 				"GET",
